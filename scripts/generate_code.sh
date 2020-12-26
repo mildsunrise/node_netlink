25697b4e731e4373195a1124e549ab22072bab11
Alba Mendez <me@alba.sh>
2020-12-26 19:07:28 +0100

    rename tools -> scripts; add "codegen" & "prepare" scripts

---
+++ scripts/generate_code.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+#ts-node scripts/generate_parser.ts types/genl.ts    lib/genl/structs.ts
+ts-node scripts/generate_parser.ts types/nl80211.ts lib/nl80211/structs.ts
+
+ts-node scripts/generate_parser.ts types/rt.ts   lib/rt/gen_structs.ts
+ts-node scripts/generate_parser.ts types/ifla.ts lib/rt/ifla.ts
